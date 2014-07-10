f39638dcee67735162b42ff5749f0c2860717d7c
Laurentiu Nicola <grayshade@gmail.com>
2014-07-10 20:09:41 +0300

    Add CinderEmpty project

---
+++ day1/0. CinderEmpty/src/CinderProjectApp.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class CairoBasicApp : public AppBasic
+{
+};
+
+CINDER_APP_BASIC(CairoBasicApp, Renderer2d)
